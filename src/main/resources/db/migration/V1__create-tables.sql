c5342d03042e8f2949b776b3bb1c8e3bc7fb0b91
caiquetgr <caiquetgr@gmail.com>
2020-11-10 21:39:13 -0300

    Making some changes to IT config and flyway script

--- src/main/resources/db/migration/V1__create-tables.sql
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -1,8 +1,6 @@
-CREATE SEQUENCE boards_id_seq;
-
 CREATE TABLE boards
 (
-    id   integer                                            NOT NULL DEFAULT nextval('boards_id_seq'),
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    id   SERIAL,
+    name character varying(50) NOT NULL,
     CONSTRAINT boards_pkey PRIMARY KEY (id)
-)
+);
